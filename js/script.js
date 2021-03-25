556687a9d13fc0cac0660867035cd5c38fade716
Pandiyan <pandiyanpotter@gmail.com>
2021-03-25 10:27:02 +0530

    Updates

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+// toggle() in classList is inverse/opposite method.
+
+const toggle = document.querySelector('.toggle')
+const navigation = document.querySelector('.navigation')
+
+toggle.addEventListener('click', () => {
+
+    toggle.classList.toggle('active')
+    navigation.classList.toggle('active')
+} ) 
